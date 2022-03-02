1ce92fc5aed73f2c11b389f89d37feaf7e79c421
lazycoder <alazycoder101@gmail.com>
2022-03-02 19:46:06 +0000

    feat: component ablychat is added

--- src/components/AblyChat.tsx
+++ src/components/AblyChat.tsx
@@ -1,68 +1,85 @@
 import React, { useEffect, useState } from 'react';
-import { useChannel } from "./AblyReactEffect";
-import styles from './AblyChatComponent.module.css';
 
-const AblyChatComponent = () => {
+import styles from './AblyChat.module.css';
 
-  let inputBox = null;
-  let messageEnd = null;
+import { useChannel } from './AblyReactEffect.js';
 
-  const [messageText, setMessageText] = useState("");
+const AblyChat = () => {
+  let inputBox: unknown = null;
+  let messageEnd: unknown = null;
+
+  const [messageText, setMessageText] = useState('');
   const [receivedMessages, setMessages] = useState([]);
   const messageTextIsEmpty = messageText.trim().length === 0;
 
-  const [channel, ably] = useChannel("chat-demo", (message) => {
-    const history = receivedMessages.slice(-199);
+  const [channel, ably] = useChannel('chat-demo', (message: string) => {
+    const history: Array<string> = receivedMessages.slice(-199);
     setMessages([...history, message]);
   });
 
-  const sendChatMessage = (messageText) => {
-    channel.publish({ name: "chat-message", data: messageText });
-    setMessageText("");
+  const sendChatMessage = (messageText: string) => {
+    channel.publish({ name: 'chat-message', data: messageText });
+    setMessageText('');
     inputBox.focus();
-  }
+  };
 
-  const handleFormSubmission = (event) => {
+  const handleFormSubmission = (event: Event) => {
     event.preventDefault();
     sendChatMessage(messageText);
-  }
+  };
 
-  const handleKeyPress = (event) => {
+  const handleKeyPress = (event: Event) => {
     if (event.charCode !== 13 || messageTextIsEmpty) {
       return;
     }
     sendChatMessage(messageText);
     event.preventDefault();
-  }
+  };
 
   const messages = receivedMessages.map((message, index) => {
-    const author = message.connectionId === ably.connection.id ? "me" : "other";
-    return <span key={index} className={styles.message} data-author={author}>{message.data}</span>;
+    const author = message.connectionId === ably.connection.id ? 'me' : 'other';
+    return (
+      <span key={index} className={styles.message} data-author={author}>
+        {message.data}
+      </span>
+    );
   });
 
   useEffect(() => {
-    messageEnd.scrollIntoView({ behaviour: "smooth" });
+    messageEnd.scrollIntoView({ behaviour: 'smooth' });
   });
 
   return (
     <div className={styles.chatHolder}>
       <div className={styles.chatText}>
         {messages}
-        <div ref={(element) => { messageEnd = element; }}></div>
+        <div
+          ref={(element) => {
+            messageEnd = element;
+          }}
+        ></div>
       </div>
       <form onSubmit={handleFormSubmission} className={styles.form}>
         <textarea
-          ref={(element) => { inputBox = element; }}
+          ref={(element) => {
+            inputBox = element;
+          }}
           value={messageText}
-          placeholder="Type a message..."
-          onChange={e => setMessageText(e.target.value)}
+          placeholder='Type a message...'
+          onChange={(e) => setMessageText(e.target.value)}
           onKeyPress={handleKeyPress}
           className={styles.textarea}
         ></textarea>
-        <button type="submit" className={styles.button} disabled={messageTextIsEmpty}>Send</button>
+        <button
+          type='submit'
+          className={styles.button}
+          disabled={messageTextIsEmpty}
+        >
+          Send
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AblyChatComponent;
+export default AblyChat;
